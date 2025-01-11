0e9de588ad470c2ac8dae32f77c8fb5ccdd34b76
Rizal <rizalnug2@gmail.com>
2025-01-11 11:43:28 +0700

    menambahkan middleware cors

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const path = require("path");
-const PORT = process.env.PORT || 8000;
+// create instance node
+const app = express();
 
+let corsOptions = {
+  origin: "http://localhost:8080",
+};
+// enable cors
+app.use(cors(corsOptions));
+// parse request of content-type - application/json
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+// parse content-type - application/x-www-form-urlencoded
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+//path for image
 app.use("/img", express.static(path.join(__dirname, "./public/img")));
-
+//Connect to database
 const db = require("./app/models");
 db.mongoose
   .connect(db.url, {})
@@ -28,6 +40,7 @@ app.get("/", (req, res) => {
 require("./app/routes/product.route")(app);
 require("./app/routes/order.route")(app);
 
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
