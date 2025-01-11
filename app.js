46a69c9b4259123fb1522ae0c4bf8ba5bc29cfda
Rizal <rizalnug2@gmail.com>
2025-01-11 09:19:11 +0700

    membuat config dan model product

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {})
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
 app.get("/", (req, res) => {
   res.json({
     message: "Welcome to ShopApp Server",
